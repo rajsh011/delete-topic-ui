979fd31a4085151f8d831345cd36271e021f1d1c
rajsh011 <rajsh011@gmail.com>
2023-05-22 17:48:02 +0530

    test changes

--- plugin.rb
+++ plugin.rb
@@ -6,6 +6,33 @@
 
 add_admin_route 'delete_topic_ui.title', 'delete-topic-ui'
 
+after_initialize do
+  require_dependency 'application_controller'
+  
+  class DeleteUserPostsController < ApplicationController
+    #  before_action :ensure_admin, only: [:delete_all_posts]
+    
+      def delete_all_posts
+   #     username = params[:username]
+         username = SiteSetting.delete_posts_for_username
+        user = User.find_by_username_or_email(username)
+    
+        if user
+          PostDestroyer.new(current_user).destroy_all_posts(user)
+          redirect_to admin_index_path, notice: "All posts by #{username} have been deleted."
+        else
+          redirect_to admin_index_path, alert: "User not found."
+        end
+      end
+  
+      def ensure_admin
+          unless current_user&.admin?
+            render json: { error: 'Unauthorized' }, status: :unauthorized
+          end
+      end
+  end
+    
+end
 Discourse::Application.routes.append do
   get '/admin/plugins/delete-topic-ui' => 'admin/plugins#index'
   post '/delete_user_posts/delete_all_posts' => 'delete_user_posts#delete_all_posts'
