91ba0a3d862111d97413b2e45ba1730588b195d5
rajsh011 <rajsh011@gmail.com>
2023-05-23 15:31:55 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,9 +7,17 @@
 add_admin_route 'delete_topic_ui.title', 'delete-topic-ui'
 
 #require_dependency File.expand_path("../app/controllers/delete_user_posts_controller.rb", __FILE__)
-after_initialize do 
-  require_relative "..app/controllers/delete_user_posts_controller.rb"
+=begin 
+  after_initialize do 
+    require_relative "..app/controllers/delete_user_posts_controller.rb"
+  end 
+=end
+
+=begin 
+after_initialize do
+  require_dependency File.expand_path("../jobs/scheduled/delete_user_posts.rb", __FILE__)
 end 
+=end
 
 Discourse::Application.routes.append do
   get '/admin/plugins/delete-topic-ui' => 'admin/plugins#index'
