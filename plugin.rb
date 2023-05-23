f7a4772ce38f00254ab4429b849f224abc8b5720
rajsh011 <rajsh011@gmail.com>
2023-05-23 13:53:46 +0530

    test

--- plugin.rb
+++ plugin.rb
@@ -5,8 +5,8 @@
 # url: https://github.com/discourse/delete-topic-ui
 
 add_admin_route 'delete_topic_ui.title', 'delete-topic-ui'
-#load File.expand_path('app/controllers/delete_user_posts_controller.rb', __dir__)
-require_dependency File.expand_path("../app/controllers/delete_user_posts_controller.rb", __FILE__)
+
+#require_dependency File.expand_path("../app/controllers/delete_user_posts_controller.rb", __FILE__)
 
 Discourse::Application.routes.append do
   get '/admin/plugins/delete-topic-ui' => 'admin/plugins#index'
