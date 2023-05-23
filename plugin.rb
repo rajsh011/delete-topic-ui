a1e7d0e26392d297f8d83a634e78d6941aaec67a
rajsh011 <rajsh011@gmail.com>
2023-05-23 12:51:42 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,6 +5,7 @@
 # url: https://github.com/discourse/delete-topic-ui
 
 add_admin_route 'delete_topic_ui.title', 'delete-topic-ui'
+load File.expand_path('app/controllers/delete_user_posts_controller.rb', __dir__)
 
 Discourse::Application.routes.append do
   get '/admin/plugins/delete-topic-ui' => 'admin/plugins#index'
