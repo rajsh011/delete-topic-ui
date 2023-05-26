3855e23a1ada64ce0936b3d12ce46f393f658c0d
rajsh011 <rajsh011@gmail.com>
2023-05-26 13:14:56 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -34,7 +34,7 @@ get '/test' => proc { |_env|
   user = User.find_by_username_or_email(username)
 
   if user
-    PostDestroyer.new(current_user).destroy_all_posts(user)
+    PostDestroyer.new(Discourse.system_user).destroy_all_posts(user)
     redirect_to admin_index_path, notice: "All posts by #{username} have been deleted."
   else
     redirect_to admin_index_path, alert: "User not found."
