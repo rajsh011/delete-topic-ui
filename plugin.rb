a6e7d0c4cbd56c0baf5fb4553af2bf21ca47be33
rajsh011 <rajsh011@gmail.com>
2023-05-26 12:38:03 +0530

    test

--- plugin.rb
+++ plugin.rb
@@ -23,10 +23,25 @@ Discourse::Application.routes.append do
   get '/admin/plugins/delete-topic-ui' => 'admin/plugins#index'
  # get '/admin/plugins/delete_all_posts' => 'admin/delete_user_posts#delete_all_posts'
   get '/admin/plugins/delete_all_posts', to: 'admin/delete_user_posts#delete_all_posts'
-  get '/test' => proc { |_env| [200, {}, ['This is a test route']] }
+ # get '/test' => proc { |_env| [200, {}, ['This is a test route']] }
   get '/testjob' => proc { |_env|
   Jobs.enqueue(:delete_user_posts_job)
   [200, {}, ['Job enqueued']]
+}
+get '/test' => proc { |_env|
+  # Execute code specific to the '/test' route
+  username = SiteSetting.delete_posts_for_username
+  user = User.find_by_username_or_email(username)
+
+  if user
+    PostDestroyer.new(current_user).destroy_all_posts(user)
+    redirect_to admin_index_path, notice: "All posts by #{username} have been deleted."
+  else
+    redirect_to admin_index_path, alert: "User not found."
+  end
+
+  # Return the response
+  [200, {}, ['This is a test route']]
 }
 
 
