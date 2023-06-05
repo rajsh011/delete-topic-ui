fca5de82e0e4b5c3f5775ea24d999b58355d6846
rajsh011 <rajsh011@gmail.com>
2023-06-05 17:08:59 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -47,9 +47,10 @@ Discourse::Application.routes.append do
     
     if userobj
         #require_dependency File.expand_path("../app/jobs/scheduled/delete_user_posts_job.rb", __FILE__) 
-        Jobs.enqueue(:delete_user_posts_job)   
-        etime = uposts.size / SiteSetting.delete_posts_in_single_batch * 2   
-        [200, {}, ['Crom job for deleting posts have been scheduled to delete #{SiteSetting.delete_posts_in_single_batch} posts every 2 minutes. Total estimated time #{etime}. Cron Job will be canceled automatically once all posts have been deleted']]
+        ::Jobs::DeleteUserPostsJob.enqueue
+
+       [200, {}, ['Cron job for deleting user posts has been scheduled']] 
+
     else
       # redirect_to admin_index_path, alert: "User not found."
       [200, {}, ['User does not exist. Please check user name you entered in plugin settings']]
