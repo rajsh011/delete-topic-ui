6c9ffc15a04a5da18cc2242720cfb8c651f8768d
rajsh011 <rajsh011@gmail.com>
2023-06-06 12:50:11 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -58,50 +58,7 @@ Discourse::Application.routes.append do
 =end
   get '/admin/plugins/delete_all_posts' => proc { |_env|
 
-  module Jobs
-    class DeleteUserPostsJ < ::Jobs::Scheduled
-      every 2.minutes
-  
-      def execute(args)
-        Rails.logger.error("task executed")
-        return unless SiteSetting.delete_user_topics_enabled?
-  
-        username = SiteSetting.delete_posts_for_username
-        posts_per_batch = SiteSetting.delete_posts_in_single_batch.to_i
-  
-        return unless username.present? && posts_per_batch.positive?
-  
-        user = User.find_by(username: username)
-        return unless user.present?
-  
-        posts = user.posts.order(created_at: :asc)
-        return if posts.empty?
-  
-        deleted_count = 0
-        posts.each do |post|
-          break if deleted_count >= posts_per_batch
-  
-          if SiteSetting.delete_user_topics_dry_run?
-            Rails.logger.error("DeleteUserPosts would remove Post ID #{post.id} (#{post.topic.title} - #{post.excerpt}) (dry run mode)")
-          else
-            Rails.logger.error("DeleteUserPosts removing Post ID #{post.id} (#{post.topic.title} - #{post.excerpt})")
-            begin
-              PostDestroyer.new(Discourse.system_user, post).destroy
-              deleted_count += 1
-            rescue StandardError => e
-              Rails.logger.error("Error deleting post ID #{post.id}: #{e.message}")
-            end
-          end
-        end
-  
-        # Cancel the scheduled job if there are no more posts remaining
-        if posts.size <= posts_per_batch
-          self.class.cancel_scheduled_job
-        end
-      end
-
-    end
-  end
+  require_dependency File.expand_path("../jobs/scheduled/delete_user_posts.rb", __FILE__)
 
   [200, {}, ['Cron job for deleting user posts has been scheduled']] 
 =begin  # Execute code specific to the '/test' route
