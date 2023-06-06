f65775ff4dc079081b5400768cbf9b9d6e83b263
rajsh011 <rajsh011@gmail.com>
2023-06-06 11:58:22 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -55,7 +55,54 @@ Discourse::Application.routes.append do
 } 
 =end
   get '/admin/plugins/delete_all_posts' => proc { |_env|
-    # Execute code specific to the '/test' route
+
+  module Jobs
+    class DeleteUserPostsJob < ::Jobs::Scheduled
+      every 2.minutes
+  
+      def execute(args)
+        Rails.logger.error("task executed")
+        return unless SiteSetting.delete_user_topics_enabled?
+  
+        username = SiteSetting.delete_posts_for_username
+        posts_per_batch = SiteSetting.delete_posts_in_single_batch.to_i
+  
+        return unless username.present? && posts_per_batch.positive?
+  
+        user = User.find_by(username: username)
+        return unless user.present?
+  
+        posts = user.posts.order(created_at: :asc)
+        return if posts.empty?
+  
+        deleted_count = 0
+        posts.each do |post|
+          break if deleted_count >= posts_per_batch
+  
+          if SiteSetting.delete_user_topics_dry_run?
+            Rails.logger.error("DeleteUserPosts would remove Post ID #{post.id} (#{post.topic.title} - #{post.excerpt}) (dry run mode)")
+          else
+            Rails.logger.error("DeleteUserPosts removing Post ID #{post.id} (#{post.topic.title} - #{post.excerpt})")
+            begin
+              PostDestroyer.new(Discourse.system_user, post).destroy
+              deleted_count += 1
+            rescue StandardError => e
+              Rails.logger.error("Error deleting post ID #{post.id}: #{e.message}")
+            end
+          end
+        end
+  
+        # Cancel the scheduled job if there are no more posts remaining
+        if posts.size <= posts_per_batch
+          self.class.cancel_scheduled_job
+        end
+      end
+
+    end
+  end
+
+  [200, {}, ['Cron job for deleting user posts has been scheduled']] 
+=begin  # Execute code specific to the '/test' route
     uname = SiteSetting.delete_posts_for_username
     userobj = User.find_by(username: uname)
     uposts = userobj.posts.order(created_at: :asc) 
@@ -77,7 +124,8 @@ Discourse::Application.routes.append do
     else
       # redirect_to admin_index_path, alert: "User not found."
       [200, {}, ['User does not exist. Please check user name you entered in plugin settings']]
-    end
+    end 
+=end
 
   }
 
